3e3fbd627eef1fca1601eea2f21318c3b627383e
Jose Salgado <Josalgado03@gmail.com>
2019-11-01 15:57:21 -0700

    Created the ProductsDb class with an add method that adds Product p to the database

---
+++ AuctionSite/Data/ProductsDb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AuctionSite.Models;
+
+namespace AuctionSite.Data
+{
+    public class ProductsDb
+    {
+
+        public Product Add(Product p, ApplicationDbContext context)
+        {
+            context.Add(p);
+            context.SaveChanges();
+            return p;        
+        }
+    }
+}
