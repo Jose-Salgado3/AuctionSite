b9d51e312d24823a7765d2691f347997bbed1dce
Jose Salgado <Josalgado03@gmail.com>
2019-12-04 02:39:09 -0800

    Migrated changes to the Product.cs. Added the PostDate property and changed the image datatype to string

---
+++ AuctionSite/Data/Migrations/20191204103014_product_PostDate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AuctionSite.Data.Migrations
+{
+    public partial class product_PostDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Image",
+                table: "Product",
+                nullable: true,
+                oldClrType: typeof(byte));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PostDate",
+                table: "Product",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PostDate",
+                table: "Product");
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "Image",
+                table: "Product",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
