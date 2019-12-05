c6b77e374c1f52fc1cebf7ab7e294e620f1e7ff6
Jose Salgado <Josalgado03@gmail.com>
2019-12-05 01:24:51 -0800

    The system is making me comit to master due to a change in the designer

--- AuctionSite/Models/Product.cs
+++ AuctionSite/Models/Product.cs
@@ -24,14 +24,13 @@ namespace AuctionSite.Models
         [StringLength(75)]
         public string Name { get; set; }
 
-
         /// <summary>
         /// A detailed description of the product to be listed
         /// </summary>
         [Required]
         [StringLength(500)]
         public string Description { get; set; }
-
+        
         /// <summary>
         /// The asking or suggested price for the product
         /// </summary>
@@ -40,13 +39,11 @@ namespace AuctionSite.Models
         [DataType(DataType.Currency)]
         public double Price { get; set; }
 
-
         /// <summary>
         /// A boolean value ( yes or no in this case) if the price is firm
         /// </summary>
         [Required]
         public bool FirmPrice { get; set; }
-
 
         /// <summary>
         /// The condition value of the product on a 1-5 scale. The value will be referenced by a drop down
@@ -56,13 +53,11 @@ namespace AuctionSite.Models
         [Range(0,5)]
         public int Condition { get; set; }
 
-
         /// <summary>
         /// A true or false value as to wether the Listing user is open to trades rather than currency
         /// </summary>
         [Required]
         public bool Trade { get; set; }
-
 
         /// <summary>
         /// The category that the item is classified in. This value will be represented by an integer value.
@@ -70,7 +65,6 @@ namespace AuctionSite.Models
         /// </summary>
         [Range(0,250)]
         public int Category { get; set; }
-
 
         /// <summary>
         /// The image stored of the product to be stored in the database.
